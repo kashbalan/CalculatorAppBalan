b0deab7a99f6f7298364e0ebe407eddfd09cc74d
kashbalan <113694415+kashbalan@users.noreply.github.com>
2023-09-10 11:26:15 -0500

    Changed the text to hints and added thee SwitchScreens screen

--- app/src/main/java/com/example/calculatorappbalan/MainActivity.java
+++ app/src/main/java/com/example/calculatorappbalan/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.calculatorappbalan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String nameToShow = intent.getStringExtra("NAME");
+
+        TextView textView = findViewById(R.id.userName);
+        textView.setText(nameToShow);
+
     }
+    public void switchScreens(View view) {
+        EditText editText = findViewById(R.id.enterfirstnumberid);
+        String name = editText.getText().toString();
+        Intent intent = new Intent(this, SecondCalcScreen.class);
+        intent.putExtra("NAME", name);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/calculatorappbalan/SecondCalcScreen.java
@@ -0,0 +1,14 @@
+package com.example.calculatorappbalan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondCalcScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
