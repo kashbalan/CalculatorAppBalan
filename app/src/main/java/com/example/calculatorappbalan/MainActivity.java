c4cd79a2bac90781e8df3058c45e18da8b111929
kashbalan <113694415+kashbalan@users.noreply.github.com>
2023-09-13 19:07:17 -0500

    Added code to compute the equation

--- app/src/main/java/com/example/calculatorappbalan/MainActivity.java
+++ app/src/main/java/com/example/calculatorappbalan/MainActivity.java
@@ -5,34 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class MainActivity extends AppCompatActivity {
-
+    String result = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
-
     }
-
-    public void calculate(View v){
-        TextView equText = (TextView)findViewById(R.id.equationid);
-        String stringEqu = equText.getText().toString();
-        for(int i = 0; i < stringEqu.length()-1; i++){
-            if(Integer.parseInt(stringEqu.substring(i,i+1)) == 0){
-                if(stringEqu.substring(i,i+1) == "+"){
-                    TextView infoTV = (TextView)findViewById(R.id.Answerid);
-                    infoTV.setText((Integer.parseInt(stringEqu.substring(0,i)) + Integer.parseInt(stringEqu.substring(i+1,stringEqu.length()))));
-                }
-            }
-        }
-
-
+    public void calculate(View v) throws ScriptException {
+        Double total = null;
+        TextView infoTV = (TextView)findViewById(R.id.equationid);
+        String info = infoTV.getText().toString();
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+        total = (double)engine.eval(info);
+        result = String.valueOf(total.doubleValue());
+        TextView infoTVAns = (TextView)findViewById(R.id.Answerid);
+        infoTVAns.setText(result);
     }
 
     public void buttonSelected(View v) {
@@ -46,98 +40,83 @@ public class MainActivity extends AppCompatActivity {
             String num8 = "8";
             String num9 = "9";
         String num0 = "0";
-
             String fullText = "";
+
             if (v.getId() == R.id.number1id) {
                 TextView infoTV = (TextView)findViewById(R.id.equationid);
                 String temp = infoTV.getText().toString();
                 fullText = temp + num1;
                 infoTV.setText(fullText);
-
-
             } if (v.getId() == R.id.number2id) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
             fullText = temp + num2;
             infoTV.setText(fullText);
-
             } if (v.getId() == R.id.number3id) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
             fullText = temp + num3;
             infoTV.setText(fullText);
-
             } if (v.getId() == R.id.number4id) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
             fullText = temp + num4;
             infoTV.setText(fullText);
-
             } if (v.getId() == R.id.number5id) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
             fullText = temp + num5;
             infoTV.setText(fullText);
-
             } if (v.getId() == R.id.number6id) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
             fullText = temp + num6;
             infoTV.setText(fullText);
-
             } if (v.getId() == R.id.number7id) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
             fullText = temp + num7;
             infoTV.setText(fullText);
-
             } if (v.getId() == R.id.number8id) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
             fullText = temp + num8;
             infoTV.setText(fullText);
-
             } if (v.getId() == R.id.number9id) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
             fullText = temp + num9;
             infoTV.setText(fullText);
-
             }
         if (v.getId() == R.id.number0id) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
             fullText = temp + num0;
             infoTV.setText(fullText);
-
         }
         if (v.getId() == R.id.plusid) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
             fullText = temp + "+";
             infoTV.setText(fullText);
-
         }
         else if (v.getId() == R.id.minusid) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
             fullText = temp + "-";
             infoTV.setText(fullText);
-
         }
         else if (v.getId() == R.id.multid) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
-            fullText = temp + "X";
+            fullText = temp + "*";
             infoTV.setText(fullText);
-
         }
         else if (v.getId() == R.id.divideid) {
             TextView infoTV = (TextView)findViewById(R.id.equationid);
             String temp = infoTV.getText().toString();
-            fullText = temp + "%";
+            fullText = temp + "/";
             infoTV.setText(fullText);
-
         }
 
         if(v.getId() == R.id.acid){
